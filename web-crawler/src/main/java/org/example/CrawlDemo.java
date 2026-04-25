c96ca26550d98fb3b7a48632a6a9dbad1747581d
M_rkquez <markus.messner99@gmail.com>
2026-04-25 17:36:34 +0200

    *added support for command line arguments (URL, depth, domain) *redirected system output to "report.md" using System.setOut

--- web-crawler/src/main/java/org/example/CrawlDemo.java
+++ web-crawler/src/main/java/org/example/CrawlDemo.java
@@ -1,14 +1,40 @@
 package org.example;
 
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class CrawlDemo {
     public static void main(String[] args) {
-        HtmlParser parser = new HtmlParser();
-        
-        String startUrl = "https://de.wikipedia.org/wiki/Kleinkatzen";
-        String domain = "wikipedia.org";
 
-        System.out.println("CRAWLER START");
-        parser.crawl(startUrl, domain, 1);
-        System.out.println("CRAWLER END");
+        if (args.length < 3) {
+            System.out.println("Gebrauch: java CrawlDemo <URL> <Tiefe> <Domain>");
+            return;
+        }
+
+        String startUrl = args[0];
+        int maxDepth = Integer.parseInt(args[1]);
+        String domain = args[2];
+
+
+        HtmlParser parser = new HtmlParser(maxDepth);
+
+        try {
+            // Leitet alle Ausgaben (System.out.println) in die Datei um
+            PrintStream fileOut = new PrintStream(new FileOutputStream("report.md"));
+            System.setOut(fileOut);
+
+            System.out.println("CRAWLER START");
+
+            parser.crawl(startUrl, domain, 1);
+            System.out.println("CRAWLER END");
+
+            fileOut.close();
+
+
+            System.err.println("Bericht wurde in report.md gespeichert.");
+
+        } catch (Exception e) {
+            System.err.println("Fehler: " + e.getMessage());
+        }
     }
 }
